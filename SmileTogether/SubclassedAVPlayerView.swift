9bac9c09d833a5058d37a11e863653f901d7bafa
rinsuki <428rinsuki+git@gmail.com>
2021-06-09 07:08:59 +0900

    fix: release build won't work
    Failed to set (contentViewController) user defined inspected property on (NSWindow): Could not instantiate class named AVPlayerView because no class named AVPlayerView was found; the class needs to be defined in source code or linked in from a library (ensure the class is part of the correct target)
    
    bypassed by use subclassed AVPlayerView

---
+++ SmileTogether/SubclassedAVPlayerView.swift
@@ -0,0 +1,12 @@
+//
+//  SubclassedAVPlayerView.swift
+//  SmileTogether
+//
+//  Created by user on 2021/06/09.
+//
+
+import Foundation
+import AVKit
+
+class SubclassedAVPlayerView: AVPlayerView {
+}
